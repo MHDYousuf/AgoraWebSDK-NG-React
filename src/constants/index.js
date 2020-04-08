a78834c716eab2c3ddbe3bad562893b049b295db
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2020-04-08 19:26:32 +0530

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const rtc = {
+  // For the local client
+  client: null,
+  // For the local audio and video tracks
+  localAudioTrack: null,
+  localVideoTrack: null,
+};
+
+export const options = {
+  // Pass your app ID here
+  appId: "<your-appid>",
+  // Pass a token if your project enables the App Certificate
+  token: null,
+};
